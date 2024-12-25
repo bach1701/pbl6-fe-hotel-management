17f44f67c2d49fbf42fcb8dd7a59d24736392df1
bach1701 <phamduybach1007@gmail.com>
2024-12-25 21:49:18 +0700

    replace image_url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = 'http://wireguard.nguyentanloc.top:8000';
-const API_BASE_URL = 'http://127.0.0.1:8000/';
-// const API_BASE_URL = 'https://demo-8000.nguyentanloc.top/';
+// const API_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'https://demo-8000.nguyentanloc.top/';
 
 export default API_BASE_URL;
